78957d0edd6e3bce137e4d3a395348b01a0f3fd7
murri06 <ruslanliakhovets@outlook.com>
2023-07-07 09:40:38 +0300

    added styles.ccs, bootstrap and font-family

--- index.php
+++ index.php
@@ -5,11 +5,15 @@
 <head>
     <title>ToDoApp</title>
     <link rel="icon" href="image/iconSmall.png">
+    <link rel="stylesheet" href="inc/styles.css">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Quicksand"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 <body>
 <main>
-    <div class="">
-        <form action="inc/task_create.php" method="post">
+    <div class="container">
+        <form action="inc/task_create.php" class="form" method="post">
             <label for="task_name">Put a name for a task
                 <input type="text" name="task_name" placeholder="Name" autocomplete="off">
             </label>
@@ -19,6 +23,9 @@
             <button type="submit">Submit</button>
         </form>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+            crossorigin="anonymous"></script>
 </main>
 </body>
 </html>
